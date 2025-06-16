1e4e1c97f04ccfeb4918e352423ce5ebc84efd3d
turbososyn <turbososyn@gmail.com>
2025-06-16 20:02:33 +0300

    fixed

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
@@ -9,54 +8,19 @@ import (
 	"time"
 )
 
-var https = flag.Bool("https", false, "whether backends support HTTPs")
-
-var serversPool = []string{
-	"localhost:8080",
-	"localhost:8081",
-	"localhost:8082",
-}
-
-type report map[string][]string
-
-func scheme() string {
-	if *https {
-		return "https"
-	}
-	return "http"
-}
+var target = flag.String("target", "http://localhost:8090", "request target")
 
 func main() {
 	flag.Parse()
-
 	client := new(http.Client)
 	client.Timeout = 10 * time.Second
 
-	res := make([]report, len(serversPool))
-	for i, s := range serversPool {
-		resp, err := client.Get(fmt.Sprintf("%s://%s/report", scheme(), s))
+	for range time.Tick(1 * time.Second) {
+		resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", *target))
 		if err == nil {
-			var data report
-			if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
-				//log.Printf("error parsing froom %s: %s", s, err)
-			} else {
-				for k, v := range data {
-					l := len(v)
-					if l > 5 {
-						l = 5
-					}
-					data[k] = v[len(v)-l:]
-				}
-				res[i] = data
-			}
+			log.Printf("response %d", resp.StatusCode)
 		} else {
-			log.Printf("error %s %s", s, err)
+			log.Printf("error %s", err)
 		}
-
-		log.Println("=========================")
-		log.Println("SERVER", i, serversPool[i])
-		log.Println("=========================")
-		data, _ := json.MarshalIndent(res[i], "", "  ")
-		log.Println(string(data))
 	}
 }
